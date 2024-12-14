839cd2fe2b0195cb63eb56c82eb3bbce39991746
rmin-mohd <mminhak@gmail.com>
2024-12-15 01:00:56 +0330

    There was a mistake now its solved

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,79 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.IOException;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    private static NotePad notePad = new NotePad();
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        boolean running = true;
+        while (running) {
+            System.out.print("\n1. Add new note");
+            System.out.print("\n2. Remove note");
+            System.out.print("\n3. show all note");
+            System.out.print("\n4. export note");
+            System.out.print("\n5. exit ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    String temp = addNoteMenu();
+                    if (temp.equals("5"))
+                        break;
+                    if (temp.equals("note-added") || temp.equals("note already exixts"))
+                        System.out.println("temp");
+                    break;
+                case 2:
+                    String temp1 = removeNoteMenu();
+                    if (temp1.equals("5"))
+                        break;
+                    if (temp1.equals("note-removed") || temp1.equals("note doesnot exixts"))
+                        System.out.println("temp1");
+                    break;
+                case 3:
+                    notePad.listNotes();
+                    break;
+                case 4:
+                    String exportResult = exportNoteMenu();
+                    if (!exportResult.equals("5"));
+                    System.out.println(exportResult);
+                case 5:
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Try again.");
+            }
         }
+
+        scanner.close();
+    }
+
+    private static String addNoteMenu() {
+        System.out.print("Enter note title: ");
+        String title = scanner.nextLine();
+        if (title.equals("5"))
+            return "5";
+        System.out.print("Enter the text:");
+        String content = scanner.nextLine();
+        return notePad.addNote(title, content);
+    }
+
+    private static String removeNoteMenu() {
+        System.out.print("Enter the note title to remove: ");
+        String title = scanner.nextLine();
+        if (title.equals("5"))
+            return "5";
+        return notePad.removeNote(title);
+    }
+
+    private static String exportNoteMenu() throws IOException {
+        System.out.println(" Enter the note title to export: ");
+        String title = scanner.nextLine();
+        if (title.equals("5"))
+            return "5";
+        notePad.exportNote(title);
+        return notePad.exportNote(title);
     }
 }
--- src/NotePad.java
+++ src/NotePad.java
@@ -1,2 +1,67 @@
-public class Note {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.time.LocalDate;
+
+public class NotePad{
+    private Map<String, String> notes;
+    private Map<String, LocalDate> creationDates;
+
+    public NotePad(){
+        notes = new HashMap<>();
+        creationDates = new HashMap<>();
+    }
+
+    public String addNote(String title, String content){
+        if(notes.containsKey(title)){
+            return "Title already exists";
+        }else{
+            notes.put(title, content);
+            creationDates.put(title, LocalDate.now());
+            return "Note added";
+        }
+    }
+
+    public String removeNote(String title){
+        if(notes.containsKey(title)){
+            notes.remove(title);
+            creationDates.remove(title);
+            return "Note removed";
+        }else{
+            return "Title does not exist";
+        }
+    }
+
+    public void listNotes(){
+        for (String title : notes.keySet()){
+            System.out.println("Titles:" + title +   "date " + creationDates.get(title));
+        }
+    }
+
+    public String exportNote(String title) throws IOException{
+        if(notes.containsKey(title)){
+            File exportDir = new File("export");
+            if(!exportDir.exists()) {
+                exportDir.mkdir();
+            }
+
+            File file = new File(exportDir, title + ".txt");
+            try (FileWriter writer = new FileWriter(file)){
+                writer.write(notes.get(title));
+            }
+            return (title + ".txt file is exported");
+        }else{
+            return "Title does not exist";
+        }
+    }
+
+    public String viewNoteContent(String title){
+        if(notes.containsKey(title)){
+            return "Content of \"" + title + "\":\n" + notes.get(title);
+        }else{
+            return "Note with the tilte \"" + title + "\" title does not exist";
+        }
+    }
 }
